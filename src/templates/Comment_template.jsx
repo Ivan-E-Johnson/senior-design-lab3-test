68c2bf8b612c60c1227c2abe36d0f6e5e99feb93
Ivan Johnson <106177326+Ivan-E-Johnson@users.noreply.github.com>
2023-04-13 10:58:31 -0500

    FIX: I think I have fixed the comments

--- src/templates/Comment_template.jsx
+++ src/templates/Comment_template.jsx
@@ -1,12 +1,31 @@
 import { Disqus, CommentCount } from 'gatsby-plugin-disqus'
 import { DiscussionEmbed } from "disqus-react"
-import React from 'react'
-const PostTemplate = (props) => {
-    let disqusConfig = props.disqusConfig
+
+import React, { useState } from "react"
+const PostTemplate = ({ postId, postTitle, postUrl }) => {
+  const [disqusIsVisible, setDisqusVisibility] = useState(false);
+
+  // Set Disqus visibility state on click.
+  const showCommentsClick = event => {
+    setDisqusVisibility(true);
+  };
+
+  let disqusConfig = {
+      url: postUrl,
+      identifier: postId,
+      title: postTitle,
+  }
+
   return (
     <>
-      <h1>New Post</h1>
-      <DiscussionEmbed {...disqusConfig} />
+      {!disqusIsVisible && (
+        <div>
+          <button onClick={showCommentsClick}>Load Comments</button>
+        </div>
+      )}
+      {disqusIsVisible && (
+        <Disqus config={disqusConfig} />
+      )}
     </>
   )
 }
