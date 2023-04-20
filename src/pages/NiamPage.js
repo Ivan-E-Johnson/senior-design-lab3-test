c3ba426f4a358f6060161df693863cc76ec33853
niam-e <niamelzein@outlook.com>
2023-04-20 10:01:53 -0500

    Add ;
    initial commit copied basic code

--- src/pages/NiamPage.js
+++ src/pages/NiamPage.js
@@ -1,16 +1,31 @@
-import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
-
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import * as styles from "../components/index.module.css"
+
+import React, { useState } from "react"
+import { Disqus, CommentCount } from 'gatsby-plugin-disqus'
+import PostTemplate from "../templates/Comment_template"
+
+const NiamPage = () => {
+    const disqusConfigCurrent = {
+        url: typeof window !== 'undefined' ? window.location.href : '',
+        identifier: 'niam-page',
+        title: "Niam's Page",
+    }
 
 const NiamPage = () => (
     <Layout>
       <h1>Hi from Niam's page</h1>
       <p>I have done so much</p>
-      <Link to="/">Go back to the homepage</Link>
+        <Link to="/">Go back to the homepage</Link>
+
+        <PostTemplate
+            postId={disqusConfigCurrent.identifier}
+            postTitle={disqusConfigCurrent.title}
+            postUrl={disqusConfigCurrent.url}
+        />
     </Layout>
   )
   
