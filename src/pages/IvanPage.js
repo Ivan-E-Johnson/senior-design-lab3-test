3a8e353bd84a74a5c7a27d1f769d1e95d208892f
Ivan Johnson <106177326+Ivan-E-Johnson@users.noreply.github.com>
2023-04-27 22:22:07 -0500

    ENH:
    Made major additions to the code includign andding a react nice carousel for the images to go in as well.

--- src/pages/IvanPage.js
+++ src/pages/IvanPage.js
@@ -1,13 +1,15 @@
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
+
 import Seo from "../components/seo"
 import * as styles from "../components/index.module.css"
 
 import React, { useState } from "react"
 import { Disqus, CommentCount } from 'gatsby-plugin-disqus'
 import PostTemplate from "../templates/Comment_template"
-
+import { Carousel } from "react-responsive-carousel"
+import "react-responsive-carousel/lib/styles/carousel.min.css"
 const IvanPage = () => {
   const disqusConfigCurrent = {
     url: typeof window !== 'undefined' ? window.location.href : '',
@@ -18,19 +20,42 @@ const IvanPage = () => {
   return (
     <Layout>
       <div className={styles.textCenter}>
-        <StaticImage
-          src="../images/IMG_5168.png"
-          loading="eager"
-          quality={40}
-          width={250}
-          formats={["auto", "webp", "avif"]}
-          alt="Ivan's profile picture"
-          style={{ marginBottom: `var(--space-3)` }}
-        />
         <h1 className={styles.name}>Ivan</h1>
         <p className={styles.jobTitle}>Computer Science Student</p>
         <p className={styles.intro}>{`I started working in a research lab at 17 and found my passion for doing image processing and data analysis. I had minimal prior experience but I was very motivated and pushed myself to learn outside of work. During the initial year of the COVID-19 pandemic I took a year off of school, which allowed me to realize that my passions required a switch from chemical engineering to computer science. Since switching majors I have reignited my passion for software development, specifically when applied to image processing, data analysis, and machine learning. Though I have taken many classes to enhance my knowledge in these areas, I also have been developing the skills needed to become a full stack engineer.`}</p>
         <Link className={styles.button} to="/">Go back to the homepage</Link>
+        <Carousel
+      infiniteLoop={true}
+      showArrows={true}
+      showStatus={false}
+      showThumbs={false}>
+      <div>
+        <StaticImage
+          src="../images/Ivan_imgs/IMG_5584.JPG"
+          alt="Image 1"
+          placeholder="blurred"
+        />
+        <p className="legend">Image 1</p>
+      </div>
+      <div>
+        <StaticImage
+          src="../images/IMG_5168.png"
+          alt="Image 2"
+          placeholder="blurred"
+        />
+        <p className="legend">Image 2</p>
+      </div>
+      <div>
+        <StaticImage
+          src="../images/Ivan_imgs/IMG_3576.jpg"
+          alt="Image 3"
+          placeholder="blurred"
+        />
+        <p className="legend">Image 3</p>
+      </div>
+    </Carousel>
+
+
         <PostTemplate
           postId={disqusConfigCurrent.identifier}
           postTitle={disqusConfigCurrent.title}
--- src/pages/NiamPage.js
+++ src/pages/NiamPage.js
@@ -4,54 +4,76 @@ import { StaticImage } from "gatsby-plugin-image"
 import { Carousel } from "react-responsive-carousel"
 import "react-responsive-carousel/lib/styles/carousel.min.css"
 
+import { Disqus, CommentCount } from 'gatsby-plugin-disqus'
+import PostTemplate from "../templates/Comment_template"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import * as styles from "../components/index.module.css"
 
-const NiamPage = () => (
-  <Layout>
-    <h1>About Niam</h1>
-    <p>
-      Hi! I am an electrical engineering student with a focus on electronic
-      circuits. My interests include signal processing and RF. I love to travel
-      and got the chance to go to Houston, Texas last year to attend the SWE
-      national conference. I learned a lot when I was there and got the chance
-      to network with so many talented engineers.
-    </p>
-    <Carousel
-      infiniteLoop={true}
-      showArrows={true}
-      showStatus={false}
-      showThumbs={false}>
-      <div>
-        <StaticImage
-          src="../images/example.png"
-          alt="Image 1"
-          placeholder="blurred"
-        />
-        <p className="legend">Image 1</p>
-      </div>
-      <div>
-        <StaticImage
-          src="../images/IMG_4992.jpg"
-          alt="Image 2"
-          placeholder="blurred"
-        />
-        <p className="legend">Image 2</p>
-      </div>
-      <div>
-        <StaticImage
-          src="../images/gatsby-icon.png"
-          alt="Image 3"
-          placeholder="blurred"
-        />
-        <p className="legend">Image 3</p>
-      </div>
-    </Carousel>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const NiamPage = () => {
+  const disqusConfigCurrent = {
+    url: typeof window !== 'undefined' ? window.location.href : '',
+    identifier: 'Niam-page',
+    title: "Niam's Page",
+  }
+  return(
+      <Layout>
+      
+      <h1>About Niam</h1>
+      <p>
+        Hi! I am an electrical engineering student with a focus on electronic
+        circuits. My interests include signal processing and RF. I love to travel
+        and got the chance to go to Houston, Texas last year to attend the SWE
+        national conference. I learned a lot when I was there and got the chance
+        to network with so many talented engineers.
+      </p>
+      <div className="image-container">
+      <Carousel
+        infiniteLoop={true}
+        showArrows={true}
+        showStatus={false}
+        showThumbs={false}>
+        <div>
+          <StaticImage
+            src="../images/Niams_Imags/NiamImg.jpg"
+            alt="Image 1"
+            placeholder="blurred"
+            className="carousel-image"
+          />
+          <p className="legend">Image 1</p>
+        </div>
+        <div>
+          <StaticImage
+            src="../images/Niams_Imags/NiamImg-1.jpg"
+            alt="Image 2"
+            placeholder="blurred"
+            className="carousel-image"
+          />
+          <p className="legend">Image 2</p>
+        </div>
+        <div>
+          <StaticImage
+            src="../images/Niams_Imags/NiamImg-2.jpeg"
+            alt="Image 3"
+            placeholder="blurred"
+            className="carousel-image"
+          />
+          <p className="legend">Image 3</p>
+        </div>
+      </Carousel>
 
+      </div>
+
+      <PostTemplate
+            postId={disqusConfigCurrent.identifier}
+            postTitle={disqusConfigCurrent.title}
+            postUrl={disqusConfigCurrent.url}
+          />
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  )
+
+}
 export const Head = () => <Seo title="Niam's Page" />
 
 export default NiamPage
